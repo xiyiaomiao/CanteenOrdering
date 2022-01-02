2ee2de6cc5635f2e2ba06004ad76b1e172112460
liushiqi <937952416@qq.com>
2022-01-02 15:48:51 +0800

    按顺序动态生成图片（未使用数据库）

--- CanteenOrdering/用户主界面.cs
+++ CanteenOrdering/用户主界面.cs
@@ -15,18 +15,47 @@ namespace CanteenOrdering
         public 用户主界面()
         {
             InitializeComponent();
+            //  generatorPictureBox();
+            generatorFlow();
         }
 
-
-
-        private void button1_Click(object sender, EventArgs e)
+        public void generatorFlow()
         {
+            int num = 4;
+            FlowLayoutPanel[] flow;
+            flow = new FlowLayoutPanel[num];
+            PictureBox[] pict;
+            pict = new PictureBox[num];
+            Label[] lab;
+            lab = new Label[num];
 
+            for(int i = 0; i < num; i++)
+            {
+                pict[i] = new System.Windows.Forms.PictureBox();
+                //    pict[i].Location = new Point(5, 10 + (i - 1) * 60);//设置图片位置  竖向排列
+                pict[i].SizeMode = PictureBoxSizeMode.Zoom;
+                pict[i].Image = Image.FromFile(@"C:\Users\木滋蔓\Pictures\test\" + (i + 1) + ".jpg");//导入图片
+                pict[i].Size = new Size(200, 200);//设置图片大小
+                pict[i].BorderStyle = BorderStyle.None;//取消边框
+                pict[i].Image.Tag = i;
+                lab[i] = new System.Windows.Forms.Label();
+                lab[i].Text = "第"+i+"个";
+                lab[i].Visible = true;
+                lab[i].AutoSize = true;
+                lab[i].Font = new System.Drawing.Font("宋体", 15F, 
+                    System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+                flow[i] = new System.Windows.Forms.FlowLayoutPanel();
+                flow[i].FlowDirection = FlowDirection.TopDown;
+                flow[i].BorderStyle = BorderStyle.FixedSingle;
+                flow[i].WrapContents = false;
+                flow[i].AutoScroll = true;
+                flow[i].AutoSize = true;
+                flow[i].Controls.Add(pict[i]);
+                flow[i].Controls.Add(lab[i]);
+                flow[i].Visible = true;
+                flowLayoutPanel1.Controls.Add(flow[i]);
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
