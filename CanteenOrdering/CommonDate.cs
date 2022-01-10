5e606a8fab895aa1a4a8ae94729677943983e65d
liushiqi <937952416@qq.com>
2022-01-10 13:46:13 +0800

    合并

--- CanteenOrdering/CommonDate.cs
+++ CanteenOrdering/CommonDate.cs
@@ -8,9 +8,9 @@ namespace CanteenOrdering
 {
     class CommonDate
     {
-               public static string ConnectionString = "server=.;" +
-          "database=食堂送餐数据库;UID=sa;PWD=123456";   //修改为你的用户名和密码
-       // public static string ConnectionString = "Data Source=LAPTOP-3CGT6SVO;Initial Catalog=食堂;Integrated Security=True";
+       //        public static string ConnectionString = "server=.;" +
+       //   "database=食堂送餐数据库;UID=sa;PWD=123456";   //修改为你的用户名和密码
+        public static string ConnectionString = "Data Source=LAPTOP-3CGT6SVO;Initial Catalog=食堂;Integrated Security=True";
 
     }
 }
